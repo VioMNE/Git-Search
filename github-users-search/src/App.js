50bcf8795c07953a15755498b3d53adc97e44c19
VioMNE <violeta.pavetic@gmail.com>
2023-04-19 15:13:50 +0200

    Git User Search

---
+++ github-users-search/src/App.js
@@ -0,0 +1,15 @@
+import { GithubProvider } from './components/GithubContext';
+import GithubSearch from './components/GithubSearch';
+import GithubResults from './components/GithubResults';
+
+const App = () => {
+  return (
+    <GithubProvider>
+      <h3>GitHub Users Searcher</h3>
+      <GithubSearch />
+      <GithubResults />
+    </GithubProvider>
+  );
+};
+
+export default App;
